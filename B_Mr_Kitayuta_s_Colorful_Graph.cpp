4aab25eaf36bdbc20fbc215ebb190df9a3d03f58
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-04-08 23:54:00 +0600

    new

--- B_Mr_Kitayuta_s_Colorful_Graph.cpp
+++ B_Mr_Kitayuta_s_Colorful_Graph.cpp
@@ -1,108 +1,80 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long 
-#define FOR(i,o,n) for (int i = o; i <= n; i++)
-#define mod 1000000007
-#define pb push_back
-#define ff first
-#define ss second
-#define pp pair<ll,ll>
-#define vl vector<ll>
-#define vpp vector<pp>
-#define sorta(v) stable_sort(v.begin(),v.end())
-#define sortd(v) stable_sort(v.begin(),v.end(),greater <> ())
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) (a*b)/gcd(a,b)
-#define INF 1000000000
-#define endl '\n'
-typedef std::complex<double> Complex;
-typedef std::valarray<Complex> CArray;
-#define fast ios::sync_with_stdio(0);cin.tie(0)
-#define all(x) x.begin(),x.end()
-#define ms(a,v) memset(a,v,sizeof a)
-#define II ({int a; scanf("%d", &a); a;})
-#define LL ({Long a; scanf("%lld", &a); a;})
-#define DD ({double a; scanf("%lf", &a); a;})
-#define mp make_pair
-#define gc getchar
-#define EPS 1e-10
-#define pi 3.1415926535897932384626433832795
-#define PI acos(-1.0)
-#define FI freopen ("in.txt", "r", stdin)
-#define FO freopen ("out.txt", "w", stdout)
-typedef unsigned long long ull;
-typedef set<int> si;
-typedef pair<string,ll>psl;
-typedef pair<double,double>pdd;
 
-#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
-#define rep(i, n)		forab (i, 0, (n) - 1)
-#define For(i, n)		forab (i, 1, n)
-#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
-#define per(i, n)		rofba (i, 0, (n) - 1)
-#define rof(i, n)		rofba (i, 1, n)
-#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+struct u
+{
+    int parent[101];
+    int size[101];
 
-#ifdef LOCAL
-#define __(args...) {dbg,args; cerr<<endl;}
-struct debugger{template<typename T> debugger& operator , (const T& v){cerr<<v<<"\t"; return *this; }}dbg;
-#define __1D(a,n) rep(i,n) { if(i) printf(" ") ; cout << a[i] ; }
-#define __2D(a,r,c,f) forab(i,f,r-!f){forab(j,f,c-!f){if(j!=f)printf(" ");cout<<a[i][j];}cout<<endl;}
-#endif
-template<class A, class B> ostream &operator<<(ostream& o, const pair<A,B>& p){ return o<<"("<<p.ff<<", "<<p.ss<<")";} //Pair print
-template<class T> ostream& operator<<(ostream& o, const vector<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Vector print
-template<class T> ostream& operator<<(ostream& o, const set<T>& v){ o<<"[";forstl(it,v)o<<*it<<", ";return o<<"]";} //Set print
-template<class T> inline void MAX(T &a , T b){ if (a < b ) a = b;}
-template<class T> inline void MIN(T &a , T b){ if (a > b ) a = b;}
+    void init()
+    {
+        memset(parent,-1,sizeof(parent));
+        for (int i = 0; i <= 100; i++)
+        {
+            size[i] = 1;
+        }
+    }
+   
+    int findParent(int x)
+    {
+        if (parent[x] == -1)
+        {
+            return x;
+        }
+        return parent[x] = findParent(parent[x]);
+    }
 
-//Fast Reader
-template<class T>inline bool read(T &x){int c=gc();int sgn=1;while(~c&&c<'0'||c>'9'){if(c=='-')sgn=-1;c=gc();}for(x=0;~c&&'0'<=c&&c<='9';c=gc())x=x*10+c-'0';x*=sgn;return ~c;}
+    void makeUnion(int a, int b)
+    {
+        int x = findParent(a);
+        int y = findParent(b);
+        if (x == y)
+        {
+            return;
+        }
+        if (size[x] > size[y])
+        {
+            parent[y] = x;
+            size[x] += size[y];
+        }
+        else
+        {
+            parent[x] = y;
+            size[y] += size[x];
+        }
+    }
 
-//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
-//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
-//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
-//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
-//freopen("input.txt", "r", stdin);
-//freopen("output.txt", "w", stdout);
-vector<int>s[101];
+    bool same(int a, int b)
+    {
+        return findParent(a) == findParent(b);
+    }
+};
 
+u graph[101];
 
 int main()
 {
-    int n,m; cin>>n>>m;
-    for(int i=1; i<=m; i++)
-    {
+    for(int i=1; i<=100; i++){
+        graph[i].init();
+    }
+    int n,m;
+    cin>>n>>m;
+    while(m--){
         int a,b,c;
         cin>>a>>b>>c;
-        s[c].push_back(a);
-        s[c].push_back(b);
+        graph[c].makeUnion(a,b);
     }
-    int q; cin>>q;
-    
-    for(int i=0; i<q; i++)
-    {
-        int a,b; cin>>a>>b;
-        int sum=0;
-        for(int i=0; i<101; i++)
-        {
-            bool chec_a=false;
-            bool chec_b=false;
-            for(int j=0; j<(int)s[i].size(); j++)
-            {
-                if(s[i][j]==a)
-                {
-                    chec_a=true;
-                }
-                if(s[i][j]==b)
-                {
-                    chec_b=true;
-                }
-            }
-            if(chec_a&&chec_b)
-            {
-                sum++;
+    int q;
+    cin>>q;
+    while(q--){
+        int a,b;
+        cin>>a>>b;
+        int ans = 0;
+        for(int i=1; i<=100; i++) {
+            if(graph[i].same(a,b)){
+                ans++;
             }
         }
-        cout<<sum<<endl;
+        cout<<ans<<endl;
     }
 }
